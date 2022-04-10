7fab544230981e5bae6b31df12107625c9302b84
Amangeldy Tomiris <70026328+Tomiris-Aman@users.noreply.github.com>
2022-04-10 14:56:22 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function handle() {
+    let inputs = document.querySelectorAll("input");
+    let input_values = [];
+    for (let i = 0; i < inputs.length; i++) {
+    input_values.push(inputs[i].value);
+    }
+    alert(input_values)
+}
+
+let button = document.querySelector('.btn');
+button.addEventListener('click', handle);
+
